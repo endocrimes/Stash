de483712d85e10e77594b8d20a7dedadfd63448b
Danielle Lancashire <dani@builds.terrible.systems>
2015-08-21 21:51:43 +0100

    Initial Commit

---
+++ Stash/Stash.h
@@ -0,0 +1,19 @@
+//
+//  Stash.h
+//  Stash
+//
+//  Created by  Danielle Lancashireon 21/08/2015.
+//  Copyright © 2015 Rocket Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stash.
+FOUNDATION_EXPORT double StashVersionNumber;
+
+//! Project version string for Stash.
+FOUNDATION_EXPORT const unsigned char StashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stash/PublicHeader.h>
+
+
