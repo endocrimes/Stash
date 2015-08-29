2c975ca4abe27566642b0ed732c8f5f21426beeb
Daniel Tomlinson <Dan@Tomlinson.io>
2015-08-29 13:47:11 +0100

    Use blocks and cleanup tests

--- Stash/Disk.swift
+++ Stash/Disk.swift
@@ -11,14 +11,45 @@ import Foundation
 public typealias DiskCacheBlock = (cache: Disk) -> ()
 public typealias DiskCacheObjectBlock = (cache: Disk, key: String, object: NSData?) -> ()
 
+private struct DiskPrivateState {
+    var maximumSizeInBytes: Double? = nil
+    var byteCount: Double = 0
+}
+
 public final class Disk {
+    private var state = DiskPrivateState()
+    private let semaphore: dispatch_semaphore_t = dispatch_semaphore_create(1)
     private let concurrentQueue: dispatch_queue_t = dispatch_queue_create("com.rocketapps.stash.disk", DISPATCH_QUEUE_CONCURRENT)
     
     /// The maximum size of the cache on disk, use nil for infinite.
-    public let maximumDiskSize: Double? = nil
+    public var maximumDiskSize: Double? {
+        get {
+            self.lock()
+            let value = state.maximumSizeInBytes
+            self.unlock()
+            
+            return value
+        }
+        set {
+            self.lock()
+            state.maximumSizeInBytes = newValue
+            self.unlock()
+        }
+    }
     
     public var byteCount: Double {
-        return 0.0
+        get {
+            self.lock()
+            let value = state.byteCount
+            self.unlock()
+            
+            return value
+        }
+        set {
+            self.lock()
+            state.byteCount = newValue
+            self.unlock()
+        }
     }
     
     init(name: String, rootPath: String) {
@@ -108,5 +139,13 @@ public final class Disk {
     
     private func async(block: dispatch_block_t) {
         dispatch_async(concurrentQueue, block)
+    }
+    
+    private func lock() {
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+    }
+    
+    private func unlock() {
+        dispatch_semaphore_signal(semaphore)
     }
 }
