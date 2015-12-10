9baff7736f066486ae7c0ea91e139e85f64c295d
Daniel Tomlinson <Dan@Tomlinson.io>
2015-12-10 19:04:20 +0000

    Actually purge on memory warnings

--- Stash/Memory.swift
+++ Stash/Memory.swift
@@ -56,14 +56,32 @@ public final class Memory {
         }
     }
     
+    public var removeAllObjectsOnMemoryWarning: Bool = true
+    
     private let concurrentQueue: dispatch_queue_t = dispatch_queue_create("com.rocketapps.stash.memory.async", DISPATCH_QUEUE_CONCURRENT)
     private let semaphore: dispatch_semaphore_t = dispatch_semaphore_create(1)
     
     private var objects: [String : NSCoding] = [String : NSCoding]()
     private var dates: [String : NSDate] = [String : NSDate]()
     private var costs: [String : Int] = [String : Int]()
+    private var observationToken: NSObjectProtocol?
     
     public init() {
+        observationToken = NSNotificationCenter.defaultCenter()
+            .addObserverForName(UIApplicationDidReceiveMemoryWarningNotification,
+                object: nil,
+                queue: NSOperationQueue(),
+                usingBlock: { [weak self] notification in
+                    guard let strongSelf = self else { return }
+                    if strongSelf.removeAllObjectsOnMemoryWarning {
+                        strongSelf.removeAllObjects()
+                    }
+                })
+    }
+    
+    deinit {
+        guard let token = observationToken else { return }
+        NSNotificationCenter.defaultCenter().removeObserver(token)
     }
     
     // MARK - Synchronous Methods
