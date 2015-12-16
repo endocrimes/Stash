1f8011ae2d812645c39eddc0fee009696ef92b21
Danielle Lancashire <dani@builds.terrible.systems>
2015-12-16 15:30:25 +0000

    Add OS X target

--- Stash/Stash.h
+++ Stash/Stash.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Rocket Apps. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Stash.
 FOUNDATION_EXPORT double StashVersionNumber;
